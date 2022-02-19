306c7743b811fe41fd845fc04dc4946314a41210
Zuza Napiórkowska <napiorkowskaz@gmail.com>
2022-02-19 23:15:10 +0100

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+const sounds = ["roar", "oink"];
+
+sounds.forEach(sound => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+
+    btn.innerText = sound;
+
+    btn.addEventListener("click", function(){
+        stopSongs();
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById("buttons").appendChild(btn);
+    
+})
+
+function stopSongs() {
+sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+    song.pause();
+    song.currentTime = 0;
+})
+}
